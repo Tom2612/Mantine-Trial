53ab0cc703c8fa7626c317f34addbd50cb0a5bd3
tom <tomcat-26@hotmail.co.uk>
2023-04-17 21:17:19 +0800

    Created carousel

--- src/pages/MyCarousel.js
+++ src/pages/MyCarousel.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Paper, Image } from '@mantine/core';
 import { Carousel } from '@mantine/carousel';
+import page1 from '../images/page1.JPG';
+import page2 from '../images/page2.JPG';
+import page3 from '../images/page3.JPG';
+import page4 from '../images/page4.JPG';
 
 export default function MyCarousel() {
   return (
-    <div>Carousel</div>
+    // <div style={{ height: '100', display: 'flex'}}>
+      <Carousel maw={1260} mx='auto' slideSize='80%' height='100%' align='center' slideGap='xl' withIndicators sx={{ flex: 1 }} loop>
+        <Carousel.Slide><Paper shadow='sm' padding='lg' radius='md' withBorder><Image src={page1} radius='md' /></Paper></Carousel.Slide>
+        <Carousel.Slide><Paper shadow='sm' padding='lg' radius='md' withBorder><Image src={page2} radius='md' /></Paper></Carousel.Slide>
+        <Carousel.Slide><Paper shadow='sm' padding='lg' radius='md' withBorder><Image src={page3} radius='md' /></Paper></Carousel.Slide>
+        <Carousel.Slide><Paper shadow='sm' padding='lg' radius='md' withBorder><Image src={page4} radius='md' /></Paper></Carousel.Slide>
+      </Carousel>
+    // </div>
   )
 }
